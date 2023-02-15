80e71388fc8733ed50ac7226b5d86701e9dfa07d
sma-dev <salahmani52@gmail.com>
2023-02-15 11:51:43 +0100

    ajoute folder examens

---
+++ resources/views/examens/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptates aperiam aliquid exercitationem ducimus quaerat doloribus sapiente necessitatibus incidunt! Nihil reprehenderit, nostrum voluptates vitae eum nulla pariatur iusto ad voluptatum!</h1>
+</body>
+</html>
